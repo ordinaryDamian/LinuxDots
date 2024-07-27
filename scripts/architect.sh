f795dd10dd5b5b39d674ba28cab15cc35aa7ecda
ordinaryDamian <94764927+ordinaryDamian@users.noreply.github.com>
2024-07-27 23:31:00 +0200

    update gnome.sh and add architect.sh

---
+++ scripts/architect.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# https://github.com/Cardiacman13/Architect?tab=readme-ov-file
+sudo pacman -S --needed git base-devel \
+  && git clone https://github.com/Gaming-Linux-FR/Architect.git ~/Architect \
+  && cd ~/Architect \
+  && chmod +x ./architect.sh \
+  && ./architect.sh
--- scripts/gnome.sh
+++ scripts/gnome.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-sudo pacman -S --noconfirm --needed gtk4 gtk3 gdm gnome-backgrounds gnome-calculator gnome-calendar gnome-characters gnome-clocks gnome-control-center gnome-disk-utility gnome-font-viewer gnome-keyring gnome-menus gnome-session gnome-settings-daemon gnome-shell gnome-shell-extensions gnome-system-monitor notepadqq gnome-weather localsearch tecla xdg-desktop-portal-gnome xdg-user-dirs-gtk gvfs gvfs-onedrive gvfs-smb gvfs-wsdd 
+sudo pacman -S --noconfirm --needed gtk4 gtk3 gdm gnome-backgrounds gnome-calculator gnome-calendar gnome-characters gnome-clocks gnome-control-center gnome-font-viewer gnome-keyring gnome-menus gnome-session gnome-settings-daemon gnome-shell gnome-shell-extensions notepadqq localsearch tecla xdg-desktop-portal-gnome xdg-user-dirs-gtk gvfs gvfs-onedrive gvfs-smb gvfs-wsdd 
 sudo pacman -S --noconfirm --needed file-roller ghex gnome-terminal gnome-tweaks seahorse sysprof gnome-chess dconf tracker3 fwupd eog
 sudo pacman -S --noconfirm --needed gimp inkscape libreoffice-fresh-en-gb libreoffice-extension-texmaths libreoffice-extension-writer2latex 
 sudo pacman -S --noconfirm --needed firefox thunderbird vlc alacritty 
 sudo pacman -S --noconfirm --needed nemo nemo-emblems nemo-fileroller nemo-image-converter nemo-preview nemo-share nemo-terminal
 
 sudo systemctl enable gdm.service
-yay -S --noconfirm --needed extension-manager gnome-shell-extension-dash-to-dock webp-pixbuf-loader
+yay -S --noconfirm --needed extension-manager
