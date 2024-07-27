8960283e1b00911c3edcaae584d76354741d2cf0
ordinaryDamian <94764927+ordinaryDamian@users.noreply.github.com>
2024-07-27 23:37:26 +0200

    cleanup

--- scripts/03theme.sh
+++ scripts/03theme.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-#sudo pacman -S gnome-themes-extra --noconfirm
-# sudo pacman -S gtk-engine-murrine gtk-engines --noconfirm
+sudo pacman -S gnome-themes-extra --noconfirm
+ sudo pacman -S gtk-engine-murrine gtk-engines --noconfirm
 sudo pacman -S tar sassc kvantum --noconfirm --needed
-#yay -S qogir-gtk-theme --noconfirm
+yay -S qogir-gtk-theme --noconfirm
 # wget https://github.com/ful1e5/Bibata_Cursor/releases/download/v2.0.6/Bibata-Modern-Classic.tar.xz
 # tar -xvf Bibata.tar.gz                # extract `Bibata.tar.gz`
 # mv Bibata-* ~/.local/share/icons/     # Install to local users
@@ -22,13 +22,13 @@ cd ..
 
 #cd ..
 
-git clone https://github.com/vinceliuice/Graphite-kde-theme
-cd Graphite-kde-theme
-sudo ./install.sh
+# git clone https://github.com/vinceliuice/Graphite-kde-theme
+# cd Graphite-kde-theme
+# sudo ./install.sh
 
-cd ..
+# cd ..
 
-git clone https://github.com/vinceliuice/Graphite-gtk-theme
-cd Graphite-gtk-theme/
-sudo ./install.sh
+# git clone https://github.com/vinceliuice/Graphite-gtk-theme
+# cd Graphite-gtk-theme/
+# sudo ./install.sh
 #cd ..
