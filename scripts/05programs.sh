59dc30e315ca1ea2f1d23d9b8f1e3f707b5314af
ordinaryDamian <94764927+ordinaryDamian@users.noreply.github.com>
2024-08-29 21:55:34 +0200

    Update 05programs.sh

--- scripts/05programs.sh
+++ scripts/05programs.sh
@@ -11,18 +11,18 @@ sudo pacman -Syyu --noconfirm
 #sudo pacman -S xorg-server xorg-apps xorg-xinit--noconfirm xorg-xman --needed
 #sudo pacman -S virtualbox-host-modules-arch virtualbox-guest-utils mesa --noconfirm --needed
 #sudo pacman -S xdg-utils iwd --noconfirm --needed
-#sudo pacman -S xdg-user-dirs --noconfirm open-vm-tools --needed
-#xdg-user-dirs-update
-# sudo systemctl enable vmtoolsd
+sudo pacman -S xdg-user-dirs --noconfirm open-vm-tools --needed
+xdg-user-dirs-update
+sudo systemctl enable vmtoolsd
 
-sudo pacman -S --noconfirm --needed curl zip unzip mlocate eza wget pkgfile openssl openssh git net-tools tldr trash-cli tar cmake bash-completion xz fzf 
+sudo pacman -S --noconfirm --needed curl zip unzip mlocate eza wget pkgfile openssl openssh git net-tools tldr trash-cli tar cmake bash-completion xz fzf moreutils
 sudo pacman -S --noconfirm --needed man cowsay btop cmatrix ripgrep fd npm python hwinfo usbutils smartmontools flatpak flatpak-kcm bat bzip2 bzip3 gzip unarchiver cpupower 
 sudo pacman -S --noconfirm --needed coreutils less findutils diffutils grep sed gawk util-linux procps-ng binutils file acpi unrar ncompress ffmpegthumbnailer fwupd nmapopenbsd-netcat
 
 # Laptop support
-sudo pacman -S --noconfirm --needed wireless_tools wpa_supplicant iw powertop 
-yay -S --needed --noconfirm bluez bluez-utils bluez-plugins
-sudo systemctl enable bluetooth.service
+#sudo pacman -S --noconfirm --needed wireless_tools wpa_supplicant iw iwd powertop connman 
+#yay -S --needed --noconfirm bluez bluez-utils bluez-plugins
+#sudo systemctl enable bluetooth.service
 
 #echo "Installing Printer suuport (CUPS)"
 #sudo pacman -S --noconfirm --needed cups cups-pdf print-manager 
@@ -32,7 +32,7 @@ sudo pacman -S --needed --noconfirm pipewire pipewire-audio pavucontrol gst-plug
 
 yay -S --needed --noconfirm gstreamer-vaapi gst-plugins-bad gst-plugins-base gst-plugins-ugly gst-plugin-pipewire gstreamer-vaapi gst-plugins-good gst-libav gstreamer libva-mesa-driver lib32-libva-mesa-driver mesa-vdpau lib32-mesa-vdpau xdg-desktop-portal-gtk xdg-desktop-portal fastfetch power-profiles-daemon lib32-pipewire hunspell hunspell-sk p7zip unrar ttf-liberation noto-fonts noto-fonts-emoji adobe-source-code-pro-fonts otf-font-awesome ttf-meslo-nerd ttf-droid duf btop fuse2fs exfatprogs fuse2 fuse3 man-db man-pages
 yay -S --needed --noconfirm vlc discord gimp obs-studio visual-studio-code-bin
-sudo pacman -S --noconfirm --needed polkit starship tree moreutils gdb lolcat thefuck 
+sudo pacman -S --noconfirm --needed polkit starship tree gdb lolcat thefuck 
 sudo pacman -S --noconfirm --needed avahi networkmanager ntfs-3g arandr gnu-netcat gparted  
 yay -S --needed --noconfirm ufw gufw ufw-extras iptables tuned
 
