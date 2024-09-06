01b4986c7804b7cb5ea11aa9af639453138af739
ordinaryDamian <94764927+ordinaryDamian@users.noreply.github.com>
2024-09-06 11:30:54 +0200

    update

--- scripts/09GUIapps.sh
+++ scripts/09GUIapps.sh
@@ -4,5 +4,5 @@ yay_cmd="yay -S --needed --noconfirm"
 
 # Install the packages using the defined command
 $yay_cmd vlc discord gimp obs-studio visual-studio-code-bin gparted gpart firefox brave-bin thunderbird obsidian signal-desktop wireshark-cli wireshark-qt 
-$yay_cmd libreoffice-fresh libreoffice-fresh-sk libreoffice-extension-texmaths packettracer gitextensions git audacity gimp filezilla atac 
+$yay_cmd libreoffice-fresh libreoffice-fresh-sk libreoffice-extension-texmaths packettracer gitextensions git audacity gimp filezilla atac bitwarden gnome-nettool
 # $yay_cmd inkscape kdenlive krita blender
