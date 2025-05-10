9abde20ea9f03996f4267aac3e39f2ae39062754
ordinaryDamian <94764927+ordinaryDamian@users.noreply.github.com>
2025-05-10 18:03:44 +0200

    add grub2 plasma config

--- scripts/08plasma.sh
+++ scripts/08plasma.sh
@@ -15,7 +15,7 @@ $yay_cmd bluedevil breeze-gtk drkonqi kde-gtk-config kdeplasma-addons kgamma kin
 $yay_cmd plasma-workspace ark kvantum dolphin dolphin-plugins flatpak ffmpegthumbs kdegraphics-thumbnailers kpipewire konsole sddm sddm-kcm systemsettings krunner plasma-firewall qalculate-qt kcharselect packagekit-qt6  #kcalc
 $yay_cmd egl-wayland kate kinit kaccounts-integration kdialog okular partitionmanager gparted yakuake plasma5-integration plasma-applet-window-buttons plasma-activities koko 
 $yay_cmd qt6-wayland kio kio-extras kio-gdrive packagekit qt5-graphicaleffects kdeconnect kdiff3 krename krusader gwenview kamera kcolorchooser svgpart elisa #kvantum-theme-materia materia-kde kvantum
-$yay_cmd kdenetwork-filesharing krdc kcron kfind kgpg spectacle 
+$yay_cmd kdenetwork-filesharing krdc kcron kfind kgpg spectacle kcm-grub2-git
 # THEME
 $yay_cmd catppuccin-konsole-theme-git sddm qt5-graphicaleffects qt5-svg qt5-quickcontrols2 sddm-catppuccin-git
 git clone --depth=1 https://github.com/catppuccin/kde catppuccin-kde && cd catppuccin-kde && ./install.sh
