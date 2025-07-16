83c1ac7a6f43a407d5f436a67f5c89e2432d9384
ordinaryDamian <94764927+ordinaryDamian@users.noreply.github.com>
2025-07-16 17:24:56 +0200

    first

--- scripts/virtmanager.sh
+++ scripts/virtmanager.sh
@@ -14,7 +14,6 @@ sudo pacman -S ebtables qemu dnsmasq vde2 bridge-utils openbsd-netcat dmidecode 
 yay -S tuned qemu-full libvirt virt-install virt-manager virt-viewer edk2-ovmf swtpm qemu-img guestfs-tools libosinfo --noconfirm --needed
 sudo sed -i 's/#unix_sock_group = "libvirt"/unix_sock_group = "libvirt"/' /etc/libvirt/libvirtd.conf
 sudo sed -i 's/#unix_sock_rw_perms = "0770"/unix_sock_rw_perms = "0770"/' /etc/libvirt/libvirtd.conf
-sudo systemctl enable libvirtd.service
 sudo usermod -a -G libvirt $(whoami)
 sudo usermod -a -G kvm $(whoami)
 # wget https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/archive-virtio/virtio-win-0.1.240-1/virtio-win-0.1.240.iso
