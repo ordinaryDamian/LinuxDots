6d59baf3f3fa4419bdaf53c24a2189d8600fe9e9
ordinaryDamian <94764927+ordinaryDamian@users.noreply.github.com>
2025-08-24 10:34:53 +0200

    Gnome update

--- scripts/08gnome.sh
+++ scripts/08gnome.sh
@@ -9,17 +9,17 @@
 
 yay_cmd="yay -S --needed --noconfirm"
 
-$yay_cmd baobab evince gdm gnome-calculator qalculate-gtk gnome-calendar gnome-characters gnome-clocks gnome-color-manager gnome-connections gnome-control-center gnome-font-viewer gnome-menus gnome-music gnome-remote-desktop gnome-session 
-$yay_cmd gnome-settings-daemon gnome-shell gnome-shell-extensions gnome-user-share grilo-plugins gvfs gvfs-google gvfs-nfs gvfs-onedrive gvfs-smb gvfs-wsdd loupe simple-scan tecla tracker3 tracker3-miners xdg-desktop-portal-gnome 
+$yay_cmd baobab decibels evince gdm gnome-calculator qalculate-gtk gnome-calendar gnome-characters gnome-clocks gnome-color-manager gnome-connections gnome-control-center gnome-disk-utility gnome-font-viewer gnome-menus gnome-music gnome-remote-desktop gnome-session 
+$yay_cmd gnome-settings-daemon gnome-shell gnome-shell-extensions gnome-user-share gnome-text-editor grilo-plugins gvfs gvfs-google gvfs-nfs gvfs-onedrive gvfs-smb gvfs-wsdd loupe simple-scan tecla tracker3 tracker3-miners xdg-desktop-portal-gnome 
 $yay_cmd gtk4 gtk3 xdg-user-dirs-gtk file-roller dconf-editor eog ghex gnome-terminal gnome-tweaks gnome-chess fwupd gnome-power-manager gnome-themes-extra polkit-gnome gtk-engines
-$yay_cmd gitg gnome-builder seahorse sysprof 
+$yay_cmd gitg gnome-builder seahorse sysprof amberol collision decoder lorem obfuscate gnome-nettool
 # Nemo File browser
-$yay_cmd nemo nemo-emblems nemo-fileroller nemo-image-converter nemo-share nemo-terminal
+#$yay_cmd nemo nemo-emblems nemo-fileroller nemo-image-converter nemo-share nemo-terminal
 # Nautilus File browser
 $yay_cmd nautilus libnautilus-extension nautilus-image-converter nautilus-share nautilus-checksums nautilus-open-any-terminal nautilus-terminal sushi
 
 $yay_cmd extension-manager
 sudo pacman -Rns --noconfirm epiphany snapshot gnome-console gnome-contacts yelp yelp-xsl gnome-logs gnome-maps gnome-music gnome-software gnome-user-docs
-sudo pacman -Rns --noconfirm gnome-system-monitor gnome-text-editor gnome-tour totem gnome-weather arandr gnome-disk-utility
+sudo pacman -Rns --noconfirm gnome-system-monitor gnome-tour totem gnome-weather arandr
 
 sudo systemctl enable gdm.service #gnome-user-share
